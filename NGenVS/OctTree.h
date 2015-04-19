6d30a9b898e7737f8249c3f67b803e9405ca1b97
MrNex <npg3253@rit.edu>
2015-04-19 00:15:46 -0400

    Declared basic memory allocation/deallocation functionality for OctTree data structure.

--- NGenVS/OctTree.h
+++ NGenVS/OctTree.h
@@ -15,7 +15,7 @@ struct OctTree_Node
 
 	//The depth of this node from the root of the tree
 	//The root has a depth of 0.
-	unsigned int depthFromRoot;
+	unsigned int depth;
 
 	//Bounds of this oct tree node
 	float left, right;		//Width
@@ -32,8 +32,69 @@ typedef struct OctTree
 	unsigned int maxDepth;		//How many subdivisions can exist
 	unsigned int maxOccupancy;	//How many occupants can an octtree have before trying to subdivide
 								//This number will be exceeded if maxDepth is reached.
-
-
 } OctTree;
+
+//Internal members
+static unsigned int defaultOccupancy = 3;
+static unsigned int defaultMaxDepth = 4;
+
+//Internal functions
+
+///
+//Allocates memory for an octtree node
+//
+//Returns:
+//	A pointer to a newly allocated, uninitialized oct tree node
+struct OctTree_Node* OctTree_Node_Allocate();
+
+///
+//Initializes an oct tree node to the given specifications
+//
+//Parameters:
+//	node: A pointer to the oct tree node to initialize
+//	leftBound: The left bound of the octtree
+//	rightBound: The right bound of the octtree
+//	bottomBound: The bottom bound of the octtree
+//	topBound: The top bound of the octtree
+//	backBound: The back bound of the octtree
+//	frontBound: The front bound of the octtree
+void OctTree_Node_Initialize(struct OctTree_Node* node, float leftBound, float rightBound, float bottomBound, float topBound, float backBound, float frontBound);
+
+///
+//Frees data allocated by an oct tree node
+//
+//Parameters:
+//	node: A pointer to the oct tree node to free
+void OctTree_Node_Free(struct OctTree_Node* node);
+
+//Functions
+
+///
+//Allocates memory for an octtree
+//
+//Returns:
+//	Pointer to a newly allocated uninitialized oct tree
+OctTree* OctTree_Allocate();
+
+///
+//Initializes an oct tree and creates a root node with the given dimensions
+//
+//Parameters:
+//	tree: A pointer to the oct tree to initialize
+//	leftBound: The left bound of the octtree
+//	rightBound: The right bound of the octtree
+//	bottomBound: The bottom bound of the octtree
+//	topBound: The top bound of the octtree
+//	backBound: The back bound of the octtree
+//	frontBound: The front bound of the octtree
+void OctTree_Initialize(OctTree* tree, float leftBound, float rightBound, float bottomBound, float topBound, float backBound, float frontBound);
+
+///
+//Frees the data allocated by an octtree.
+//Does not free any of the data contained within the octtree!
+//
+//Parameters:
+//	tree: A pointer to the octtree to free
+void OctTree_Free(OctTree* tree);
 
 #endif
